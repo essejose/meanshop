bc0aa674dba169c52ec4ad4568f9419beae8e501
essejose <ze_pk2@hotmail.com>
2016-07-17 13:49:48 -0300

    criando controladores: delete

--- client/app/products/products.controller.js
+++ client/app/products/products.controller.js
@@ -1,7 +1,20 @@
 'use strict';
 
 angular.module('meanshopApp')
+
   .controller('ProductsCtrl', function ($scope, Products) {
     $scope.products = Products.query();
 
+  })
+
+  .controller('ProductsCtrl', function ($scope, $state, $stateParams, Product) {
+    $scope.products = Products.get({id:$stateParams.id});
+
+    $scope.deleteProduct = function(){
+
+    	Products.delete($scope.product);
+    	$state.go('products');
+    }
+
   });
+
