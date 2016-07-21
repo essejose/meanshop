083a1223ff4df8f429b1035f2b16f91b3562f1f7
essejose <ze_pk2@hotmail.com>
2016-07-20 21:37:58 -0300

    criando novas rotas

--- client/app/products/products.js
+++ client/app/products/products.js
@@ -5,7 +5,15 @@ angular.module('meanshopApp')
     $stateProvider
       .state('products', {
         url: '/products',
-        templateUrl: 'app/products/products.html',
+        templateUrl: 'app/products/products-list.html',
+        controller: 'ProductsCtrl'
+      })
+
+      .state('newProduct', {
+        url: '/products/new',
+        templateUrl: 'app/products/products-new.html',
         controller: 'ProductsCtrl'
       });
+
+
   });
